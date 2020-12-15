8ec333d33bf242726f00bdce7791edf931fe50d3
wangqi <wangqi@xinc818.group>
2020-12-15 15:37:23 +0800

    设计索引流程

---
+++ indexer-service/src/main/java/cn/sliew/indexer/chain/FullIndexCommand.java
@@ -0,0 +1,12 @@
+package cn.sliew.indexer.chain;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+
+public class FullIndexCommand implements Command {
+
+    @Override
+    public boolean execute(Context context) throws Exception {
+        return false;
+    }
+}
