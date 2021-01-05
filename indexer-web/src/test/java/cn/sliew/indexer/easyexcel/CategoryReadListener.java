047185bc16a936409232fdeaabd22ca0d6b473d2
wangqi <wangqi@xinc818.group>
2021-01-05 19:09:23 +0800

    类目数据构建

--- indexer-web/src/test/java/cn/sliew/indexer/easyexcel/CategoryReadListener.java
+++ indexer-web/src/test/java/cn/sliew/indexer/easyexcel/CategoryReadListener.java
@@ -5,20 +5,33 @@ import cn.sliew.indexer.dao.mapper.CategoryMapper;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
 import org.springframework.util.StringUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Slf4j
 public class CategoryReadListener extends AnalysisEventListener<EasyExcelTestCase.Category> {
 
     private CategoryMapper categoryMapper;
+    private ExecutorService executorService;
 
     public CategoryReadListener(CategoryMapper categoryMapper) {
         this.categoryMapper = categoryMapper;
+        ThreadPoolExecutorFactoryBean factoryBean = new ThreadPoolExecutorFactoryBean();
+        factoryBean.setMaxPoolSize(50);
+        factoryBean.setCorePoolSize(50);
+        factoryBean.setQueueCapacity(256);
+        factoryBean.setKeepAliveSeconds(10);
+        factoryBean.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        factoryBean.afterPropertiesSet();
+        this.executorService = factoryBean.getObject();
     }
 
     @Override
     public void invoke(EasyExcelTestCase.Category category, AnalysisContext analysisContext) {
-        saveData(category);
+        this.executorService.execute(() -> saveData(category));
     }
 
     @Override
@@ -61,8 +74,8 @@ public class CategoryReadListener extends AnalysisEventListener<EasyExcelTestCas
         category1.setCategoryId(category.getSed_id());
         category1.setCategoryName(category.getSed_name());
         category1.setParentCategoryId(category.getId());
-        category1.setCategoryIdPath(category.getId() + "|" + category.getSed_id());
-        category1.setCategoryNamePath(category.getName() + "|" + category.getSed_name());
+        category1.setCategoryIdPath(category.getId() + "/" + category.getSed_id());
+        category1.setCategoryNamePath(category.getName() + "/" + category.getSed_name());
         categoryMapper.insertOrUpdateSelective(category1);
     }
 
@@ -71,8 +84,8 @@ public class CategoryReadListener extends AnalysisEventListener<EasyExcelTestCas
         category1.setCategoryId(category.getTh_id());
         category1.setCategoryName(category.getTh_namefrom());
         category1.setParentCategoryId(category.getSed_id());
-        category1.setCategoryIdPath(category.getId() + "|" + category.getSed_id() + "|" + category.getTh_id());
-        category1.setCategoryNamePath(category.getName() + "|" + category.getSed_name() + "|" + category.getTh_namefrom());
+        category1.setCategoryIdPath(category.getId() + "/" + category.getSed_id() + "/" + category.getTh_id());
+        category1.setCategoryNamePath(category.getName() + "/" + category.getSed_name() + "/" + category.getTh_namefrom());
         categoryMapper.insertOrUpdateSelective(category1);
     }
 
@@ -81,8 +94,8 @@ public class CategoryReadListener extends AnalysisEventListener<EasyExcelTestCas
         category1.setCategoryId(category.getFr_id());
         category1.setCategoryName(category.getFr_namefrom());
         category1.setParentCategoryId(category.getTh_id());
-        category1.setCategoryIdPath(category.getId() + "|" + category.getFr_id());
-        category1.setCategoryNamePath(category.getName() + "|" + category.getFr_namefrom());
+        category1.setCategoryIdPath(category.getId() + "/" + category.getSed_id() + "/" + category.getTh_id() + "/" + category.getFr_id());
+        category1.setCategoryNamePath(category.getName() + "/" + category.getSed_name() + "/" + category.getTh_namefrom() + "/" + category.getFr_namefrom());
         categoryMapper.insertOrUpdateSelective(category1);
     }
 
@@ -90,9 +103,9 @@ public class CategoryReadListener extends AnalysisEventListener<EasyExcelTestCas
         Category category1 = new Category();
         category1.setCategoryId(category.getFv_id());
         category1.setCategoryName(category.getFv_namefrom());
-        category1.setParentCategoryId(category.getTh_id());
-        category1.setCategoryIdPath(category.getId() + "|" + category.getFr_id() + "|" + category.getSed_id() + "|" + category.getTh_id() + "|" + category.getFv_id());
-        category1.setCategoryNamePath(category.getName() + "|" + category.getFr_namefrom() + "|" + category.getSed_name() + "|" + category.getTh_namefrom() + "|" + category.getFv_namefrom());
+        category1.setParentCategoryId(category.getFr_id());
+        category1.setCategoryIdPath(category.getId() + "/" + category.getSed_id() + "/" + category.getTh_id() + "/" + category.getFr_id() + "/" + category.getFv_id());
+        category1.setCategoryNamePath(category.getName() + "/" + category.getSed_name() + "/" + category.getTh_namefrom() + "/" + category.getFr_namefrom() + "/" + category.getFv_namefrom());
         categoryMapper.insertOrUpdateSelective(category1);
     }
 }
--- indexer-web/src/test/java/cn/sliew/indexer/easyexcel/EasyExcelTestCase.java
+++ indexer-web/src/test/java/cn/sliew/indexer/easyexcel/EasyExcelTestCase.java
@@ -2,22 +2,26 @@ package cn.sliew.indexer.easyexcel;
 
 import cn.sliew.indexer.ApplicationTestCase;
 import cn.sliew.indexer.dao.mapper.CategoryMapper;
+import com.alibaba.excel.EasyExcel;
 import lombok.Getter;
 import lombok.Setter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql("/sql/category.sql")
+//@Sql("/sql/category.sql")
 //@Transactional
 public class EasyExcelTestCase extends ApplicationTestCase {
 
     @Autowired
     private CategoryMapper categoryMapper;
 
-    @Test
+//    @Test
     public void parseExcel() throws Exception {
-//        EasyExcel.read(new ClassPathResource("/taobao_category_0731.xls").getInputStream(), Category.class, new CategoryReadListener(categoryMapper)).sheet().doRead();
+        EasyExcel.read(new ClassPathResource("/taobao_category_0731.xls").getInputStream(), Category.class, new CategoryReadListener(categoryMapper)).sheet().doRead();
+
+        Thread.sleep(1000 * 60 * 60);
     }
 
     @Getter
